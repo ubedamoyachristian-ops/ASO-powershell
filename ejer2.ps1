81ffc60db6093c45723ba82b56c54ff1c862f83c
ubedamoyachristian-ops <ubedamoyachristian@gmail.com>
2026-01-09 13:54:09 +0100

    Añadimos los ejercicios 2 y 3

---
+++ ejer2.ps1
@@ -0,0 +1,21 @@
+﻿Clear-Host
+[int] $num1 = Read-Host "Introduce el primer número"
+[int] $num2 = Read-Host "Introduce el segundo número"
+
+$suma = $num1 + $num2
+$resta = $num1 - $num2
+$multiplicacion = $num1 * $num2
+
+if ($num2 -ne 0) {
+    $division = $num1 / $num2
+    $resto = $num1 % $num2
+} else {
+    $division = "No se puede dividir entre 0"
+    $resto = "No se puede calcular el resto"
+}
+
+Write-Host "Suma: $suma"
+Write-Host "Resta: $resta"
+Write-Host "Multiplicación: $multiplicacion"
+Write-Host "División: $division"
+Write-Host "Resto: $resto"
---
+++ ejer3.ps1
@@ -0,0 +1,7 @@
+﻿Clear-Host
+[int] $horas = Read-Host "Introduce el número de horas trabajadas"
+[int] $costeHora = Read-Host "Introduce el coste por hora"
+
+$salario = $horas * $costeHora
+
+Write-Host "El salario a pagar es: $salario €"
