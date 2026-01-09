f7a58af74258661f4339e06a515a687111e78c31
ubedamoyachristian-ops <ubedamoyachristian@gmail.com>
2026-01-09 13:40:15 +0100

    Añadimos ejercicio1

---
+++ ejer1.ps1
@@ -0,0 +1,9 @@
+﻿Clear-Host
+$nombre = $env:USERNAME
+Write-Host "Hola $nombre"
+$directorio = $env:USERPROFILE
+Write-Host = "Tu directorio de trabajo es $directorio"
+$nombre_dominio = $env:USERDOMAIN
+Write-Host = "Perteneces al dominio $nombre_dominio"
+$nombre_equipo = $env:COMPUTERNAME
+Write-Host = "Tu equipo se llama $nombre_equipo"
