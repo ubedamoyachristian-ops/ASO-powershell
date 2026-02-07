8884ba8941cf5bb2c3c4f80a9544511cf214c40a
ubedamoyachristian-ops <ubedamoyachristian@gmail.com>
2026-02-07 11:46:54 +0100

    Add files via upload

---
+++ Perfiles_moviles/perfiles.ps1
@@ -0,0 +1,10 @@
+﻿$personalOU = "OU=PERSONAL,OU=Empresa,DC=empresa,DC=local"
+
+$usuariosPersonal = Get-ADUser -Filter * -SearchBase $personalOU
+
+foreach ($u in $usuariosPersonal) {
+    $samAccount = $u.SamAccountName
+    $profilePath = "\\$env:COMPUTERNAME\Empresa_users$\" + $samAccount + "\Perfil"
+    
+    Set-ADUser -Identity $samAccount -ProfilePath $profilePath
+}
